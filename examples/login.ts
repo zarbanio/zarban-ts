aedc3091aad6179f9950b3b363490cab98b39f8f
arashalaei <arashalaei22@gmail.com>
2024-12-21 02:28:13 +0330

    upgrade login example

--- examples/login.ts
+++ examples/login.ts
@@ -1,4 +1,5 @@
-import { Wallet } from "zarban";
+import { Wallet, ZarbanUtils } from "zarban";
+const { withErrorHandler } = ZarbanUtils;
 
 async function loginExample() {
   // Create and configure the Configuration object
@@ -6,48 +7,44 @@ async function loginExample() {
     basePath: "https://testwapi.zarban.io",
   });
 
-  // Create an instance of the authApi
+  // Create an instance of the BaseAPI using the Configuration
   const authApi = new Wallet.AuthApi.AuthApi(cfg);
 
   // Prepare the login request data
-  const loginRequest = Wallet.LoginRequestFromJSON({
+  const loginRequest: Wallet.LoginRequest = {
     email: "user@example.com",
     password: "your_secure_password",
-  });
+  };
 
-  try {
-    // Call the login API
-    const response = await authApi.loginWithEmailAndPassword({
-      loginRequest: loginRequest,
-    });
-    console.log("Login successful!");
-    console.log(`Token: ${response.token}`);
-
-    // After successful login, you can set the access token for future authenticated requests
-    /*
-    const newCfg = new Wallet.Configuration({
-      basePath: "https://testwapi.zarban.io",
-      accessToken: response.token,
-    });
-    */
-
-    return response;
-  } catch (error) {
-    const modelError = Wallet.instanceOfModelError(error);
-    if (modelError) {
-      console.log(
-        `Exception when calling authApi->loginWithEmailAndPassword: ${error}`
-      );
-      console.log(`Error message: ${error.msg}`);
-    } else {
-      console.error("Unexpected error:", error);
+  const loginWithHandler = withErrorHandler<Wallet.JwtResponse>(
+    "Wallet",
+    () => authApi.loginWithEmailAndPassword(loginRequest),
+    (response) => {
+      console.log("Login successful!");
+      console.log(`Token: ${response.data.token}`);
     }
-    throw error;
+  );
+
+  const [response, error] = await loginWithHandler();
+  if (error) {
+    // you can do some addition works with error here!
+    return error;
   }
+  // After successful login, you can set the access token for future authenticated requests
+  /*
+  const newCfg = new Wallet.Configuration({
+    basePath: "https://testwapi.zarban.io",
+    accessToken: response.token,
+  });
+  */
+  return response;
 }
 
 if (require.main === module) {
-  loginExample()
-    .then(() => process.exit(0))
-    .catch(() => process.exit(1));
+  loginExample().then((result) => {
+    if (result instanceof Error) {
+      process.exit(1);
+    }
+    process.exit(0);
+  });
 }
