2c8d6064ae0cd120e20ca3eba35ecf819fdaea9a
arashalaei <arashalaei22@gmail.com>
2024-12-20 06:32:34 +0330

    migrate to axios

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Wallet = exports.Service = void 0;
 const tslib_1 = require("tslib");
-exports.Service = tslib_1.__importStar(require("./service/src/index.js"));
-exports.Wallet = tslib_1.__importStar(require("./wallet/src/index.js"));
+exports.Service = tslib_1.__importStar(require("./service"));
+exports.Wallet = tslib_1.__importStar(require("./wallet"));
 //# sourceMappingURL=index.js.map
--- fix-exports.cjs
+++ fix-exports.cjs
@@ -3,15 +3,20 @@ const path = require("path");
 
 const paths = ["service", "wallet"];
 for (const p of paths) {
-  // Path to the generated api.ts file
-  const apiFilePath = path.resolve(__dirname, `./src/${p}/src/apis/index.ts`);
+  // Path to the generated index.ts file
+  const apiFilePath = path.resolve(__dirname, `./src/${p}/api.ts`);
   // Read the file content
   let apiFileContent = fs.readFileSync(apiFilePath, "utf-8");
   // Use regex to transform `export * from` to `export * as NameApi from`
   apiFileContent = apiFileContent.replace(
-    /export \* from ['"]\.\/([A-Za-z]+)Api['"];/g,
+    /export \* from '\.\/api\/([a-z\-]+)-api';/g,
     (match, p1) => {
-      return `export * as ${p1}Api from "./${p1}Api";`;
+      // Capitalize the first letter of the API name
+      const apiName = p1
+        .split("-")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join("");
+      return `export * as ${apiName}Api from './api/${p1}-api';`;
     }
   );
   // Write the updated content back to the file
